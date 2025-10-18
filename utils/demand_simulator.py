4d4db540f638318e60e354566cab1b52b4e2cd4f
Aarathi <aarathinair06@gmail.com>
2025-10-18 21:03:50 +0530

    Delete utils directory

--- utils/demand_simulator.py
+++
@@ -1,8 +0,0 @@
-import random
-
-def simulate_demand():
-    """
-    Simulates market demand dynamically.
-    Returns one of ['Low', 'Medium', 'High'] randomly.
-    """
-    return random.choice(['Low', 'Medium', 'High'])
